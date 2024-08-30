88c01df2263847c7de458544b89255b279486d65
Damian Lewandowski <d.lewandowski94@onet.pl>
2024-08-30 21:23:14 +0200

    Added function to operator Buttons

--- app.js
+++ app.js
@@ -4,18 +4,39 @@ let screen = document.querySelector(".screen");
 
 let firstNumber = "";
 let secondNumber = "";
-let selectedNumber = "";
+
+let selectedOperator = null;
 
 numberBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (firstNumber === "") {
-      firstNumber = btn.value;
+    if (selectedOperator == null) {
+      firstNumber += btn.value;
       screen.innerHTML = firstNumber;
     } else {
-      secondNumber = btn.value;
+      secondNumber += btn.value;
       screen.innerHTML = secondNumber;
     }
     console.log("Pierwszy: " + firstNumber);
     console.log("Drugi: " + secondNumber);
   });
+});
+
+let operator = document.querySelectorAll(".operator");
+
+operator.forEach((oper) => {
+  oper.addEventListener("click", catchOperetor);
+});
+
+// catching selected operator //
+function catchOperetor(oper) {
+  selectedOperator = oper.target.value;
+  console.log(selectedOperator);
+}
+
+function calculate() {}
+
+let equality = document.getElementById("result");
+
+equality.addEventListener("click", () => {
+  console.log(equality.value);
 });
